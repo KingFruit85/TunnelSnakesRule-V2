ee80ad45651542167edfc2c1b49d0ef71ef810b5
christopher.long <christopher.long@ovo.com>
2024-02-01 23:20:19 +0000

    image uploads sucl

--- src/app/api/avatar/upload/route.ts
+++ src/app/api/avatar/upload/route.ts
@@ -1,24 +1,26 @@
-import { addImageToSession } from '@/app/lib/actions';
-import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
-import { NextResponse } from 'next/server';
- 
+import { addImageToSession } from "@/app/lib/actions";
+import { handleUpload, type HandleUploadBody } from "@vercel/blob/client";
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request): Promise<NextResponse> {
   const body = (await request.json()) as HandleUploadBody;
+  const clientPayload =
+      "clientPayload" in body.payload ? body.payload.clientPayload : null;
 
   try {
     const jsonResponse = await handleUpload({
       body,
       request,
       onBeforeGenerateToken: async (
-        pathname: string,
+        pathname: string
         /* clientPayload?: string, */
       ) => {
         // Generate a client token for the browser to upload the file
         // ⚠️ Authenticate and authorize users before generating the token.
         // Otherwise, you're allowing anonymous uploads.
- 
+
         return {
-          allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif'],
+          allowedContentTypes: ["image/jpeg", "image/png", "image/gif"],
           tokenPayload: JSON.stringify({
             // optional, sent to your server on upload completion
             // you could pass a user id from auth, or a value from clientPayload
@@ -30,32 +32,29 @@ export async function POST(request: Request): Promise<NextResponse> {
         // ⚠️ This will not work on `localhost` websites,
         // Use ngrok or similar to get the full upload flow
 
-        console.log('blob upload completed', blob, tokenPayload);
-        const clientPayload = 'clientPayload' in body.payload ? body.payload.clientPayload : null;
-        if (clientPayload) {
-          await addImageToSession(blob.url, clientPayload);
-          console.log('added image to session', blob.url, clientPayload)
-        } else {
-          console.log('no client payload')
-          throw new Error('No id provided, image is not associated with a session, result or player');
-        }
-        
- 
+        console.log("blob upload completed", blob, tokenPayload);
+
         try {
-          // Run any logic after the file upload completed
-          // const { userId } = JSON.parse(tokenPayload);
-          // await db.update({ avatar: blob.url, userId });
+          if (clientPayload) {
+            await addImageToSession(blob.url, clientPayload);
+            console.log("added image to session", blob.url, clientPayload);
+          } else {
+            console.log("no client payload");
+            throw new Error(
+              "No id provided, image is not associated with a session, result or player"
+            );
+          }
         } catch (error) {
-          throw new Error('Could not update user');
+          throw new Error("Could not update user");
         }
       },
     });
- 
+
     return NextResponse.json(jsonResponse);
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error).message },
-      { status: 400 }, // The webhook will retry 5 times waiting for a 200
+      { status: 400 } // The webhook will retry 5 times waiting for a 200
     );
   }
 }
