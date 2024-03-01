b414931ad4bb478c3ef68c510e7d261b6a12536e
christopher.long <christopher.long@ovo.com>
2024-03-01 10:18:46 +0000

    thoubleshooting

--- src/app/ui/sessions/currentSession/currentSessionImages.tsx
+++ src/app/ui/sessions/currentSession/currentSessionImages.tsx
@@ -14,7 +14,7 @@ export default function CurrentSessionImages({
   return (
     <div className="flex gap-1 px-2 py-2 items-center flex-col">
       <div className="flex items-center ">
-        {session.imageurl && session.imageurl.length > 0 && (
+        {/* {session.imageurl && session.imageurl.length > 0 && (
           <div className="flex gap-1 flex-wrap">
             {session?.imageurl?.map((image, index) => (
               <Image
@@ -27,7 +27,7 @@ export default function CurrentSessionImages({
             ))}
           </div>
           
-        )}
+        )} */}
       </div>
     </div>
   );
